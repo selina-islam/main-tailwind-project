588eb359bd424fc84f53eb663c50d69aeffd607d
selina-islam <digitalselina@gmail.com>
2024-07-08 13:31:37 +0800

    Tailwind Project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["*"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        cover: "url('/images/coverbg .png')",
+      },
+    },
+  },
+  plugins: [],
+}
+
